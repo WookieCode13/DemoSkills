42b4677876d5946719e208c074c4010ba761df89
WookieCode13 <telecaster13@gmail.com>
2025-12-29 15:47:06 -0500

    feat: add Pay, TaxCalculator, and Report APIs with health + swagger and pipeline support

---
+++ apis/TaxCalculatorAPI/Program.cs
@@ -0,0 +1,64 @@
+using System;
+using Microsoft.OpenApi.Models;
+using Serilog;
+
+var buildBranch = Environment.GetEnvironmentVariable("BUILD_BRANCH") ?? "local";
+
+// Bootstrap Serilog early
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((ctx, services, cfg) => cfg
+        .ReadFrom.Configuration(ctx.Configuration)
+        .Enrich.FromLogContext()
+        .WriteTo.Console());
+
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Title = "TaxCalculatorAPI",
+            Version = $"v1 - {buildBranch}",
+            Description = $"Build branch: {buildBranch}"
+        });
+    });
+
+    var app = builder.Build();
+
+    var enableSwagger = builder.Configuration.GetValue<bool>("EnableSwagger", false) || app.Environment.IsDevelopment();
+    if (enableSwagger)
+    {
+        app.UseSwagger(c =>
+        {
+            c.RouteTemplate = "taxes/swagger/{documentName}/swagger.json";
+        });
+        app.UseSwaggerUI(c =>
+        {
+            c.RoutePrefix = "taxes/swagger";
+            c.SwaggerEndpoint("v1/swagger.json", "TaxCalculatorAPI v1");
+        });
+    }
+
+    app.UseSerilogRequestLogging();
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    Log.Information("Starting TaxCalculatorAPI");
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "TaxCalculatorAPI terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
