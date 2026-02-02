b7b30ce449eaefc6eb0a3aae7673de77444d7320
WookieCode13 <telecaster13@gmail.com>
2026-02-02 16:22:18 -0500

    Add Alembic migration step to ECS deploy

--- apis/EmployeeAPI/Domain/Entities/AuditLog.cs
+++ apis/EmployeeAPI/Domain/Entities/AuditLog.cs
@@ -2,5 +2,13 @@ namespace EmployeeAPI.Domain.Entities;
 
 public class AuditLog
 {
-    
+    public Guid Id { get; set; }
+    public string EntityType { get; set; } = string.Empty;
+    public Guid EntityId { get; set; }
+    public string Action { get; set; } = string.Empty;
+    public DateTimeOffset OccurredUtc { get; set; }
+    public string PerformedBy { get; set; } = "system";
+    public string? ChangedFields { get; set; }
+    public string? Note { get; set; }
+    public string? CorrelationId { get; set; }
 }
