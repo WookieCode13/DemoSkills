fe0009e6d22137c28fd96479b92c71d02f3e2338
WookieCode13 <telecaster13@gmail.com>
2026-02-04 16:51:49 -0500

    Fix record validation annotations

--- apis/EmployeeAPI/Contracts/Employees/CreateEmployeeRequest.cs
+++ apis/EmployeeAPI/Contracts/Employees/CreateEmployeeRequest.cs
@@ -3,10 +3,10 @@ using System.ComponentModel.DataAnnotations;
 namespace EmployeeAPI.Contracts.Employees;
 
 public record CreateEmployeeRequest(
-    [property: Required, StringLength(100)] string FirstName,
-    [property: Required, StringLength(100)] string LastName,
-    [property: Required, EmailAddress, StringLength(255)] string Email,
-    [property: StringLength(20)] string? Phone,
+    [Required, StringLength(100)] string FirstName,
+    [Required, StringLength(100)] string LastName,
+    [Required, EmailAddress, StringLength(255)] string Email,
+    [StringLength(20)] string? Phone,
     DateTime? DateOfBirth,
-    [property: Required, StringLength(11)] string SSN
+    [Required, StringLength(11)] string SSN
 );
--- apis/EmployeeAPI/Contracts/Employees/PatchEmployeeRequest.cs
+++ apis/EmployeeAPI/Contracts/Employees/PatchEmployeeRequest.cs
@@ -3,9 +3,9 @@ using System.ComponentModel.DataAnnotations;
 namespace EmployeeAPI.Contracts.Employees;
 
 public record PatchEmployeeRequest(
-    [property: StringLength(100)] string? FirstName,
-    [property: StringLength(100)] string? LastName,
-    [property: EmailAddress, StringLength(255)] string? Email,
-    [property: StringLength(20)] string? Phone,
+    [StringLength(100)] string? FirstName,
+    [StringLength(100)] string? LastName,
+    [EmailAddress, StringLength(255)] string? Email,
+    [StringLength(20)] string? Phone,
     DateTime? DateOfBirth
 );
