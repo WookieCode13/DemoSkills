ff72900104ed6736c770c5d1fdcf13c265c0255a
WookieCode13 <telecaster13@gmail.com>
2026-02-04 16:48:29 -0500

    Simplify audit changed_fields jsonb migration

--- apis/EmployeeAPI/Migrations/V2026020102_ChangeAuditLogChangedFieldsToJsonb.cs
+++ apis/EmployeeAPI/Migrations/V2026020102_ChangeAuditLogChangedFieldsToJsonb.cs
@@ -9,15 +9,7 @@ public sealed class V2026020102_ChangeAuditLogChangedFieldsToJsonb : Migration
     public override void Up()
     {
         Log.Information("Changing audit_log.changed_fields to jsonb");
-        Execute.Sql("""
-            ALTER TABLE audit_log
-            ALTER COLUMN changed_fields TYPE jsonb USING
-              CASE
-                WHEN changed_fields IS NULL THEN NULL
-                WHEN btrim(changed_fields) LIKE '[%' THEN changed_fields::jsonb
-                ELSE to_jsonb(string_to_array(changed_fields, ','))
-              END;
-            """);
+        Execute.Sql("ALTER TABLE audit_log ALTER COLUMN changed_fields TYPE jsonb;");
     }
 
     public override void Down()
